6581ff99f07e294dea251b5745172c7611ac2202
ᐸʸᵉᵒᵘᵈᵉᵛ/ᐳ <antonioalx66@gmail.com>
2025-06-19 23:12:04 -0600

    feat: implement NotFound component and configure application routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { NotFound } from './auth/not-found/not-found';
+import { authGuard } from './auth/guards/auth-guard';
+import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
+import { UserProfile } from './auth/user-profile/user-profile';
 
-export const routes: Routes = [];
+// Define the application's routes
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: Login,
+    title: 'Login',
+  },
+  {
+    path: 'register',
+    component: Register,
+    title: 'Register',
+  },
+  {
+    path: 'profile',
+    component: UserProfile,
+    canActivate: [authGuard], // Protect this route
+    title: 'User Profile',
+  },
+  {
+    path: '',
+    redirectTo: '/data', // Default route redirects to data
+    pathMatch: 'full',
+  },
+  {
+    path: '**', // Wildcard route for 404
+    component: NotFound,
+    title: '404 - Not Found',
+  },
+];
